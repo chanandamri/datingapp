fa6a2abf36fd548e01bca0332b4bbe02337ef30f
chanandamri <damrica@gmail.com>
2023-03-19 22:41:38 +0200

    dropdown works

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
