d42bc88b14faa77d8e7f3c671509f3578fcf7bf9
chanandamri <damrica@gmail.com>
2023-04-16 00:54:38 +0300

    end of section 9

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -13,7 +13,7 @@
         public string Gender { get; set; }
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
-        public string Intrests { get; set; }
+        public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
         public List<Photo> Photos { get; set; } = new List<Photo>();
